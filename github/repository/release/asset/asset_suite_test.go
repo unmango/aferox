883969daafa999611c61a45419c5ee90429670b9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-01-12 21:35:26 -0600

    Copy over all the stuff from the other repo

---
+++ github/repository/release/asset/asset_suite_test.go
@@ -0,0 +1,21 @@
+package asset_test
+
+import (
+	"testing"
+
+	"github.com/google/go-github/v68/github"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/unmango/aferox/github/internal"
+)
+
+var client *github.Client
+
+func TestRelease(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Asset Suite")
+}
+
+var _ = BeforeSuite(func() {
+	client = internal.DefaultClient()
+})
