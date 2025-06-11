998e270b02ef4501307309d317e4e3c58a78ca05
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-11 00:29:54 -0500

    Arguably, a file is a file and not a directory

--- readonly.go
+++ readonly.go
@@ -1,11 +1,22 @@
 package aferox
 
 import (
+	"os"
 	"syscall"
 )
 
 // ReadOnlyFile can be embedded to assist with defining read-only implementations of afero.File
 type ReadOnlyFile struct{}
+
+// Readdir implements afero.File.
+func (s *ReadOnlyFile) Readdir(count int) ([]os.FileInfo, error) {
+	return nil, syscall.EPERM
+}
+
+// Readdirnames implements afero.File.
+func (s *ReadOnlyFile) Readdirnames(n int) ([]string, error) {
+	return nil, syscall.EPERM
+}
 
 // Seek implements afero.File.
 func (r *ReadOnlyFile) Seek(offset int64, whence int) (int64, error) {
