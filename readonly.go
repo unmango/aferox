d115c651b47a897d27ccc2143a039a9a1c2d5061
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-11 21:41:16 -0500

    Readonly fs too

--- readonly.go
+++ readonly.go
@@ -3,9 +3,60 @@ package aferox
 import (
 	"os"
 	"syscall"
+	"time"
+
+	"github.com/spf13/afero"
 )
 
-// ReadOnlyFile can be embedded to assist with defining read-only implementations of afero.File
+// ReadOnlyFs can be embedded in structs to assist with defining read-only implementations of afero.Fs
+type ReadOnlyFs struct{}
+
+// Chmod implements afero.Fs.
+func (r *ReadOnlyFs) Chmod(name string, mode os.FileMode) error {
+	return syscall.EPERM
+}
+
+// Chown implements afero.Fs.
+func (r *ReadOnlyFs) Chown(name string, uid int, gid int) error {
+	return syscall.EPERM
+}
+
+// Chtimes implements afero.Fs.
+func (r *ReadOnlyFs) Chtimes(name string, atime time.Time, mtime time.Time) error {
+	return syscall.EPERM
+}
+
+// Create implements afero.Fs.
+func (r *ReadOnlyFs) Create(name string) (afero.File, error) {
+	return nil, syscall.EPERM
+}
+
+// Mkdir implements afero.Fs.
+func (r *ReadOnlyFs) Mkdir(name string, perm os.FileMode) error {
+	return syscall.EPERM
+}
+
+// MkdirAll implements afero.Fs.
+func (r *ReadOnlyFs) MkdirAll(path string, perm os.FileMode) error {
+	return syscall.EPERM
+}
+
+// Remove implements afero.Fs.
+func (r *ReadOnlyFs) Remove(name string) error {
+	return syscall.EPERM
+}
+
+// RemoveAll implements afero.Fs.
+func (r *ReadOnlyFs) RemoveAll(path string) error {
+	return syscall.EPERM
+}
+
+// Rename implements afero.Fs.
+func (r *ReadOnlyFs) Rename(oldname string, newname string) error {
+	return syscall.EPERM
+}
+
+// ReadOnlyFile can be embedded in structs to assist with defining read-only implementations of afero.File
 type ReadOnlyFile struct{}
 
 // Readdir implements afero.File.
