10c9d4956e03818b95206e47db00dd725f054225
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-13 13:03:19 -0500

    More tests and more bugs

--- protofs/grpc/v1alpha1/e2e_test.go
+++ protofs/grpc/v1alpha1/e2e_test.go
@@ -1,9 +1,11 @@
 package protofsv1alpha1_test
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net"
+	"os"
 	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -52,6 +54,30 @@ var _ = Describe("E2e", func() {
 		actual, err := fs.Open("test.txt")
 		Expect(err).NotTo(HaveOccurred())
 		data, err := io.ReadAll(actual)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(string(data)).To(Equal("testing text"))
+	})
+
+	It("should open a file with the create flag", func() {
+		file, err := client.OpenFile("test.txt", os.O_CREATE, os.ModePerm)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(file.Name()).To(Equal("test.txt"))
+
+		stat, err := fs.Stat("test.txt")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(stat.Name()).To(Equal("test.txt"))
+	})
+
+	It("should open an existing file", func(ctx context.Context) {
+		err := afero.WriteFile(fs, "test.txt", []byte("testing text"), os.ModePerm)
+		Expect(err).NotTo(HaveOccurred())
+
+		file, err := client.Open("test.txt")
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(file.Name()).To(Equal("test.txt"))
+		data, err := io.ReadAll(file)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(string(data)).To(Equal("testing text"))
 	})
