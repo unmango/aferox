e6a10754586ba588f8b3e0a9458f65f6fadfca35
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-22 18:28:55 -0500

    Refactors CutPrefix tests for clarity
    Reorders arguments in CutPrefix test table to improve readability and make the tests easier to understand.

--- mapped/path_test.go
+++ mapped/path_test.go
@@ -10,15 +10,16 @@ import (
 var _ = Describe("Path", func() {
 	FDescribe("CutPrefix", func() {
 		DescribeTable("should cut matching prefixes",
-			func(s, prefix, expected string) {
+			func(prefix, s, expected string) {
 				actual, found := mapped.CutPrefix(s, prefix)
 
 				Expect(found).To(BeTrueBecause("The prefix was found"))
 				Expect(actual).To(Equal(expected))
 			},
-			Entry(nil, "test/test.txt", "test", "/test.txt"),
-			Entry(nil, "/test/test.txt", "/test", "/test.txt"),
-			Entry(nil, "test/test.txt", "/test", "/test.txt"),
+			Entry(nil, "test", "test/test.txt", "test.txt"),
+			Entry(nil, "test", "/test/test.txt", "/test.txt"),
+			Entry(nil, "/test", "test/test.txt", "test.txt"),
+			Entry(nil, "/test", "/test/test.txt", "/test.txt"),
 		)
 	})
 })
